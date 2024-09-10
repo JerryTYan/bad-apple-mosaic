76860edfc87c4aba0c84cb7cab211fdb2087490c
JerryTYan <jerryy668@gmail.com>
2024-09-10 02:43:09 -0700

    Upload Window

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import customtkinter as ctk
 
 app = ctk.CTk()
 app.title("Bad Appleify")
-app.geometry("400x200")
+app.config(padx=50,pady=50)
 
-app.grid_columnconfigure(0, weight=1)
-app.grid_rowconfigure((0,1), weight=1)
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("blue")
 
-chooseImageBtn = ctk.CTkButton(master=app, text="Choose Image")
-chooseImageBtn.grid(row=0, column=0, padx=20, pady=20)
+app.grid_columnconfigure((0,1,2), weight=1, uniform="col")
+app.grid_rowconfigure((0,1,2,3), weight=1, uniform="row")
 
-uploadBtn = ctk.CTkButton(master=app, text="Upload")
-uploadBtn.grid(row=1, column=0, padx=20, pady=20)
+titleLbl = ctk.CTkLabel(master=app, text="Bad Appleify Any Image!", font=("Segoe UI", 20, "bold"), anchor="center")
+titleLbl.grid(row=0, column=1, pady=20, sticky="ew")
+
+selectImageBtn = ctk.CTkButton(master=app, text="Select Image", border_width=1, border_color="#dfe6e9", fg_color="#6c5ce7", hover_color="#5f27cd", command="selectFileHandler")
+selectImageBtn.grid(row=1, column=0, pady=20, sticky="ew")
+fileNameLbl = ctk.CTkLabel(master=app, text="No file selected", fg_color="#636e72", anchor="w", corner_radius=5)
+fileNameLbl.grid(row=1, column=1, columnspan=2, pady=20, sticky="ew")
+
+uploadBtn = ctk.CTkButton(master=app, text="Upload", border_width=1, border_color="#dfe6e9", fg_color="#6c5ce7", hover_color="#5f27cd", command="uploadFileHandler")
+uploadBtn.grid(row=3, column=1, pady=20, sticky="ew")
 
 app.mainloop()
